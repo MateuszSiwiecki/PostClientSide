ea0455741c37af1ef5a7b6a92bd27fc1acd7a2f7
Koczo Koczownik <mateux111@gmail.com>
2021-01-18 00:18:47 +0100

    Add project files.

---
+++ PostClientSide/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json.Serialization;
+
+namespace PostClientSide
+{
+    class Program
+    {
+        static void Main(string[] args) => 
+            Console.WriteLine(
+                new HttpClient()
+                    .GetAsync("https://localhost:44332/gallery")    //proszę dopasować do aktualnie otwartego portu przy testowaniu
+                    .Result
+                    .Content
+                    .ReadAsStringAsync()
+                    .Result);
+    }
+}
